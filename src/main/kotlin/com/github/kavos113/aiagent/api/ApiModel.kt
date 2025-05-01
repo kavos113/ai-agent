2901c8a25c61c842c8a835221898eba88be8d838
kavos113 <kohei.hadano@gmail.com>
2025-05-01 13:48:59 +0900

    handlers

--- src/main/kotlin/com/github/kavos113/aiagent/api/ApiModel.kt
+++ src/main/kotlin/com/github/kavos113/aiagent/api/ApiModel.kt
@@ -1,7 +1,10 @@
 package com.github.kavos113.aiagent.api
 
+import com.anthropic.models.messages.Model
+import com.openai.models.ChatModel
+
 sealed class ApiModel {
-    data class AnthropicModel(val model: com.anthropic.models.messages.Model): ApiModel()
-    data class OpenAiModel(val model: com.openai.models.models.Model): ApiModel()
+    data class AnthropicModel(val model: Model): ApiModel()
+    data class OpenAiModel(val model: ChatModel): ApiModel()
     data class OpenRouterModel(val model: String): ApiModel()
 }
